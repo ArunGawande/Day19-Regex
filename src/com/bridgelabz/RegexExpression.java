59b5dcf837e75698aeabb7e6892d6aad5e310f30
Arun <arungawande1995@gmail.com>
2022-07-08 08:20:50 +0530

    Added: Validate Password Pattern Atleast 1 Upper Case

--- src/com/bridgelabz/RegexExpression.java
+++ src/com/bridgelabz/RegexExpression.java
@@ -5,26 +5,27 @@ import java.util.regex.Pattern;
 
 public class RegexExpression 
 {
-         static boolean validate(String pattern, String text) {
-		
+        
+	static boolean validate(String pattern, String text) {
+
 		return Pattern.compile(pattern).matcher(text).matches();
 	}
 
 	public static void main(String[] args) {
 
-		String pattern = "^[a-zA-Z0-9]{8,}$";
+		String pattern = "^[A-Z]{1}[a-zA-Z0-9]{8,}$";
 
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Please Enter value =>");
+		System.out.println("Please Enter Password =>");
 
 		String lastName = scanner.next();
 
 		boolean isValid = validate(pattern, lastName);
 
 		if (isValid) {
-			System.out.println("This is valid Email ID .");
+			System.out.println("This is valid Password .");
 		} else {
-			System.out.println("Not a valid ID");
+			System.out.println("Not a valid Password");
 		}
 	}
 }
